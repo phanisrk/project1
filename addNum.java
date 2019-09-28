b2a9e674da0ee461f858d2215559bcac4fb2d658
phanisrk <phanisrk@gmail.com>
2019-09-28 17:34:09 +0530

    two files

--- addNum.java
+++ addNum.java
@@ -6,7 +6,7 @@ int a=3;
 int b=4;
 int c=10;
 System.out.println(a+b+c);
-
+System.out.println(a*c);
 }
 }
 
---
+++ calNew.java
@@ -0,0 +1,11 @@
+public class calNew{
+ 
+public static void main(String[] args){
+
+int p=3;
+int q=4;
+int t=10;
+System.out.println(t*(p+q));
+
+}
+}
